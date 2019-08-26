4403bc2e0e04617f4ccf908813542f52da8979b2
Roseee123 <m10702150@gapps.ntust.edu.tw>
2019-08-26 17:15:00 +0800

    fix model name

--- routes/guest.js
+++ routes/guest.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var account = require('../models/guest');
+var guest = require('../models/guest');
  
 var router = express.Router();
  
-// 獲取 /account 請求
+// 獲取 /guest 請求
 router.route('/')
     // 取得所有資源
     .get(function (req, res) {
-        account.items(req, function (err, results, fields) {
+        guest.items(req, function (err, results, fields) {
             if (err) {
                 res.sendStatus(500);
                 return console.error(err);
@@ -23,11 +23,11 @@ router.route('/')
         });
     });
  
-// 獲取如 /account/1 請求
+// 獲取如 /guest/1 請求
 router.route('/:id')
     // 取得指定的一筆資源
     .get(function (req, res) {
-        account.item(req, function (err, results, fields) {
+        guest.item(req, function (err, results, fields) {
             if (err) {
                 res.sendStatus(500);
                 return console.error(err);
